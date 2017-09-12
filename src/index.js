7cec03a606a44ff5268fd5f53b753f1a5b271736
Daniel Alexander Fermin Nilsen <dafn@outlook.com>
2017-09-12 18:42:58 +0200

    added stuff

--- src/index.js
+++ src/index.js
@@ -1,56 +1,77 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
-const url = require('url')
+document.ondragover = e => e.preventDefault();
+document.ondrop = e => e.preventDefault();
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win
+var imageTypes = ['.jpg', '.png', '.jpeg', '.gif'],
+	extention;
 
-function createWindow() {
-    // Create the browser window.
-    win = new BrowserWindow({ width: 800, height: 600 })
+const args = require('electron').remote.process.argv,
+	path = require('path'),
+	holder = document.getElementById('holder'),
+	fs = require('fs');
 
-    // and load the index.html of the app.
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }))
+fs.readdir(args[2], (err, content) => {
 
-    // Open the DevTools.
-    // win.webContents.openDevTools()
+	files = iterator(content);
+	zoom = zoom(100);
 
-    // Emitted when the window is closed.
-    win.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        win = null
-    })
+	do { extention = files.next(); }
+	while (imageTypes.indexOf(path.extname(extention)) === -1);
+	holder.style.backgroundImage = `url('${args[2]}${extention}')`;
+
+	document.onkeydown = event => {
+		if (event.code === 'ArrowRight') {
+			do { extention = files.next(); }
+			while (imageTypes.indexOf(path.extname(extention)) === -1);
+
+			holder.style.backgroundImage = `url('${args[2]}${extention}')`;
+			zoom.reset();
+
+		} else if (event.code === 'ArrowLeft') {
+			do { extention = files.prev(); }
+			while (imageTypes.indexOf(path.extname(extention)) === -1);
+
+			holder.style.backgroundImage = `url('${args[2]}${extention}')`;
+			zoom.reset();
+		}
+	}
+	document.onmousewheel = event => {
+		(event.wheelDelta > 0) ? zoom.up() : zoom.down();
+	}
+});
+
+iterator = array => {
+	let nextIndex = 0;
+
+	return {
+		next: () => {
+			nextIndex++;
+			return (nextIndex < array.length) ? array[nextIndex] : array[nextIndex = 0]
+		},
+		prev: () => {
+			nextIndex--;
+			return (nextIndex >= 0) ? array[nextIndex] : array[nextIndex = array.length - 1]
+		}
+	};
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+zoom = value => {
+	let size = value;
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
-})
-
-app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-        createWindow()
-    }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-
+	return {
+		up: () => {
+			size += 10;
+			holder.style.height = `${size}%`;
+			holder.style.width = `${size}%`;
+		},
+		down: () => {
+			size -= 10;
+			holder.style.height = `${size}%`;
+			holder.style.width = `${size}%`;
+		},
+		reset: () => {
+			size = value;
+			holder.style.height = '100%';
+			holder.style.width = '100%';
+		}
+	}
+}
