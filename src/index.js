04978053d5d8743a2cd8e52c9b3e1aaf1f4f693c
Daniel Alexander Fermin Nilsen <dafn@outlook.com>
2017-09-13 12:45:33 +0200

    added webm and mp4 support

--- src/index.js
+++ src/index.js
@@ -1,36 +1,54 @@
 document.ondragover = e => e.preventDefault();
 document.ondrop = e => e.preventDefault();
 
-var imageTypes = ['.jpg', '.png', '.jpeg', '.gif'],
-	extention;
+var imageTypes = ['.jpg', '.png', '.jpeg', '.gif', '.webm', '.mp4'],
+	file;
 
 const args = require('electron').remote.process.argv,
 	path = require('path'),
 	holder = document.getElementById('holder'),
 	fs = require('fs');
 
+
 fs.readdir(args[2], (err, content) => {
 
 	files = iterator(content);
-	zoom = zoom(100);
+	zoom = zoom(90);
 
-	do { extention = files.next(); }
-	while (imageTypes.indexOf(path.extname(extention)) === -1);
-	holder.style.backgroundImage = `url('${args[2]}${extention}')`;
+	do { file = files.next(); }
+	while (imageTypes.indexOf(path.extname(file)) === -1);
+
+	document.getElementsByTagName('title')[0].innerText = file;
+
+	if (path.extname(file) === '.webm' || path.extname(file) === '.mp4') {
+		showVideo(`${args[2]}${file}`);
+	} else {
+		showImage(`${args[2]}${file}`);
+	}
 
 	document.onkeydown = event => {
 		if (event.code === 'ArrowRight') {
-			do { extention = files.next(); }
-			while (imageTypes.indexOf(path.extname(extention)) === -1);
+			do { file = files.next(); }
+			while (imageTypes.indexOf(path.extname(file)) === -1);
 
-			holder.style.backgroundImage = `url('${args[2]}${extention}')`;
+			if (path.extname(file) === '.webm' || path.extname(file) === '.mp4') {
+				showVideo(`${args[2]}${file}`);
+			} else {
+				showImage(`${args[2]}${file}`);
+			}
+
 			zoom.reset();
 
 		} else if (event.code === 'ArrowLeft') {
-			do { extention = files.prev(); }
-			while (imageTypes.indexOf(path.extname(extention)) === -1);
+			do { file = files.prev(); }
+			while (imageTypes.indexOf(path.extname(file)) === -1);
 
-			holder.style.backgroundImage = `url('${args[2]}${extention}')`;
+			if (path.extname(file) === '.webm' || path.extname(file) === '.mp4') {
+				showVideo(`${args[2]}${file}`);
+			} else {
+				showImage(`${args[2]}${file}`);
+			}
+
 			zoom.reset();
 		}
 	}
@@ -38,6 +56,20 @@ fs.readdir(args[2], (err, content) => {
 		(event.wheelDelta > 0) ? zoom.up() : zoom.down();
 	}
 });
+
+showVideo = path => {
+	document.getElementsByTagName('title')[0].innerText = file;
+	document.getElementsByTagName('video')[0].src = path;
+	document.getElementsByTagName('img')[0].style.opacity = '0';
+	document.getElementsByTagName('video')[0].style.opacity = '1';
+}
+
+showImage = path => {
+	document.getElementsByTagName('title')[0].innerText = file;
+	document.getElementsByTagName('img')[0].src = path;
+	document.getElementsByTagName('video')[0].style.opacity = '0';
+	document.getElementsByTagName('img')[0].style.opacity = '1';
+}
 
 iterator = array => {
 	let nextIndex = 0;
@@ -60,18 +92,18 @@ zoom = value => {
 	return {
 		up: () => {
 			size += 10;
-			holder.style.height = `${size}%`;
-			holder.style.width = `${size}%`;
+			document.getElementById('id').style.height = `${size}%`;
+			document.getElementById('id').style.width = `${size}%`;
 		},
 		down: () => {
 			size -= 10;
-			holder.style.height = `${size}%`;
-			holder.style.width = `${size}%`;
+			document.getElementById('id').style.height = `${size}%`;
+			document.getElementById('id').style.width = `${size}%`;
 		},
 		reset: () => {
 			size = value;
-			holder.style.height = '100%';
-			holder.style.width = '100%';
+			document.getElementById('id').style.height = `${size}%`;
+			document.getElementById('id').style.width = `${size}%`;
 		}
 	}
 }
