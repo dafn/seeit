1e8e8a55ed8042fe2d7397f85e68093c3650f146
Daniel Alexander Fermin Nilsen <dafn@outlook.com>
2017-12-14 10:42:34 +0100

    added multiple window support on mac

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow } = require('electron'),
 	{ INDEX } = require('./src/js/constants'),
 	path = require('path');
 
-let win;
+let windows = []
 
 createWindow = () => {
 
@@ -12,7 +12,7 @@ createWindow = () => {
 
 	// global.sharedObj = { filepath: process.argv[INDEX], platform: process.platform };
 
-	win = new BrowserWindow({
+	let win = new BrowserWindow({
 		minWidth: 128, minHeight: 128, autoHideMenuBar: true, titleBarStyle: 'hidden',
 		darkTheme: true, backgroundColor: '#21252B', show: false
 	})
@@ -21,6 +21,7 @@ createWindow = () => {
 	win.on('closed', () => win = null);
 
 	// win.webContents.openDevTools();
+	windows.push(win);
 }
 
 app.on('ready', createWindow);
@@ -28,6 +29,7 @@ app.on('will-finish-launching', () => {
 	app.on('open-file', (event, path) => {
 		event.preventDefault();
 		global.sharedObj = { filepath: path, platform: process.platform };
+		if (windows.length !== 0) { createWindow(); }
 	});
 });
 
@@ -36,7 +38,7 @@ app.on('window-all-closed', () => {
 })
 
 app.on('activate', () => {
-	if (win === null) {
+	if (!!win) {
 		createWindow();
 	}
 })
