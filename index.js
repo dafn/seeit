98a9cc675a8ea21a22360ace5f8e0f7392bb1c79
Daniel Nilsen <dafn@Daniels-MacBook-Pro.local>
2021-10-28 00:04:56 +0200

    clean a bit

--- index.js
+++ index.js
@@ -1,54 +1,74 @@
-const { app, BrowserWindow } = require('electron'),
-	{ INDEX } = require('./src/js/constants')
+const { app, BrowserWindow } = require("electron");
+const { INDEX } = require("./src/js/constants");
 
-let windows = []
+let windows = [];
 
 createWindow = () => {
+  console.log("hi there");
 
-	if (process.platform == 'win32') {
-		global.sharedObj = { filepath: process.argv[INDEX], platform: process.platform }
+  if (process.platform == "win32") {
+    global.sharedObj = {
+      filepath: process.argv[INDEX],
+      platform: process.platform,
+    };
 
-		let win32_win = new BrowserWindow({
-			minWidth: 128, minHeight: 128, autoHideMenuBar: true, frame: false,
-			darkTheme: true, backgroundColor: '#21252B', show: process.env.NODE_ENV === 'development',
-			webPreferences: { nodeIntegration: true }
-		})
+    let win32_win = new BrowserWindow({
+      minWidth: 128,
+      minHeight: 128,
+      autoHideMenuBar: true,
+      frame: false,
+      darkTheme: true,
+      backgroundColor: "#21252B",
+      show: process.env.NODE_ENV === "development",
+      webPreferences: { nodeIntegration: true, contextIsolation: false },
+    });
 
-		win32_win.loadURL(`file://${__dirname}/src/view/index.html`)
-		win32_win.on('closed', () => win = null)
+    win32_win.loadURL(`file://${__dirname}/src/view/index.html`);
+    win32_win.on("closed", () => (win = null));
 
-		process.env.NODE_ENV === 'development' &&
-			win32_win.webContents.openDevTools()
+    process.env.NODE_ENV === "development" &&
+      win32_win.webContents.openDevTools();
+  } else {
+    console.log("hi there 2");
 
-	} else {
+    if (process.env.NODE_ENV === "development") {
+      global.sharedObj = {
+        filepath: process.argv[INDEX],
+        platform: process.platform,
+      };
+      console.log("hi there 3");
+    }
 
-		if (process.env.NODE_ENV === 'development')
-			global.sharedObj = { filepath: process.argv[INDEX], platform: process.platform }
+    let win = new BrowserWindow({
+      minWidth: 128,
+      minHeight: 128,
+      autoHideMenuBar: true,
+      titleBarStyle: "hidden",
+      darkTheme: true,
+      backgroundColor: "#21252B",
+      show: process.env.NODE_ENV === "development",
+      webPreferences: { nodeIntegration: true, contextIsolation: false },
+    });
 
-		let win = new BrowserWindow({
-			minWidth: 128, minHeight: 128, autoHideMenuBar: true, titleBarStyle: 'hidden',
-			darkTheme: true, backgroundColor: '#21252B', show: process.env.NODE_ENV === 'development',
-			webPreferences: { nodeIntegration: true }
-		})
+    win.loadURL(`file://${__dirname}/src/view/index.html`);
+    win.on("closed", () => (win = null));
 
-		win.loadURL(`file://${__dirname}/src/view/index.html`)
-		win.on('closed', () => win = null)
+    console.log("hi there 4");
 
-		process.env.NODE_ENV === 'development' &&
-			win.webContents.openDevTools()
+    process.env.NODE_ENV === "development" && win.webContents.openDevTools();
 
-		windows.push(win)
-	}
-}
+    windows.push(win);
+  }
+};
 
-process.platform == 'darwin' &&
-	app.on('will-finish-launching', () => {
-		app.on('open-file', (event, path) => {
-			event.preventDefault()
-			global.sharedObj = { filepath: path, platform: 'darwin' }
-			app.isReady() && createWindow()
-		})
-	})
+process.platform == "darwin" &&
+  app.on("will-finish-launching", () => {
+    app.on("open-file", (event, path) => {
+      event.preventDefault();
+      global.sharedObj = { filepath: path, platform: "darwin" };
+      app.isReady() && createWindow();
+    });
+  });
 
-app.on('ready', createWindow)
-app.on('window-all-closed', app.quit)
+app.on("ready", createWindow);
+app.on("window-all-closed", app.quit);
--- src/js/index.js
+++ src/js/index.js
@@ -1,147 +1,161 @@
-const remote = require('electron').remote,
-	win = remote.getCurrentWindow(),
-	sharedObj = remote.getGlobal('sharedObj'),
-	path = require('path'),
-	fs = require('fs'),
-	helper = require('../js/helpers'),
-	{ TYPES_VIDEO } = require('../js/constants')
+const remote = require("electron").remote;
+const win = remote.getCurrentWindow();
+const sharedObj = remote.getGlobal("sharedObj");
+const path = require("path");
+const fs = require("fs");
+const helper = require("../js/helpers");
+const { TYPES_VIDEO } = require("../js/constants");
 
-const img = document.getElementById('image'),
-	vid = document.getElementById('video'),
-	title = document.getElementById('title'),
-	dirname = path.dirname(sharedObj.filepath) + '/',
-	transform = helper.transform()
+const img = document.getElementById("image");
+const vid = document.getElementById("video");
+const title = document.getElementById("title");
+const dirname = path.dirname(sharedObj.filepath) + "/";
+const transform = helper.transform();
 
 let first = true,
-	file = path.basename(sharedObj.filepath)
+  file = path.basename(sharedObj.filepath);
 
-if (sharedObj.platform == 'win32') {
-	const titlebarButtons = document.getElementById('win-titlebar-btns'),
-		titlebar = document.getElementById('titlebar')
+if (sharedObj.platform == "win32") {
+  const titlebarButtons = document.getElementById("win-titlebar-btns"),
+    titlebar = document.getElementById("titlebar");
 
-	titlebarButtons.style.visibility = 'visible'
-	titlebarButtons.innerHTML = `
+  titlebarButtons.style.visibility = "visible";
+  titlebarButtons.innerHTML = `
 			<button id="min-btn" class="windows_toolbar_buttons">-</button>
 			<button id="max-btn" class="windows_toolbar_buttons">+</button>
 			<button id="close-btn" class="windows_toolbar_buttons">x</button>
-		`
+		`;
 
-	titlebar.classList.add("win")
-	
-	document.getElementById("close-btn").addEventListener("click", e => win.close())
-	document.getElementById("min-btn").addEventListener("click", e => win.minimize())
-	document.getElementById("max-btn").addEventListener("click", e =>
-		win.isMaximized() ? win.unmaximize() : win.maximize()
-	)
+  titlebar.classList.add("win");
+
+  document
+    .getElementById("close-btn")
+    .addEventListener("click", (e) => win.close());
+  document
+    .getElementById("min-btn")
+    .addEventListener("click", (e) => win.minimize());
+  document
+    .getElementById("max-btn")
+    .addEventListener("click", (e) =>
+      win.isMaximized() ? win.unmaximize() : win.maximize()
+    );
 }
 
 if (TYPES_VIDEO.indexOf(path.extname(file).toLowerCase()) !== -1) {
-	title.innerText = file
+  title.innerText = file;
 
-	vid.src = `${dirname}${file}`
-	vid.style.visibility = 'visible'
-	vid.style.zIndex = '1'
+  vid.src = `${dirname}${file}`;
+  vid.style.visibility = "visible";
+  vid.style.zIndex = "1";
 
-	vid.onloadeddata = () => {
-		if (first) {
-			first = false
+  vid.onloadeddata = () => {
+    if (first) {
+      first = false;
 
-			size = helper.setWindowSize({
-				wi: vid.videoWidth, hi: vid.videoHeight,
-				ws: window.screen.availWidth, hs: window.screen.availHeight
-			})
+      size = helper.setWindowSize({
+        wi: vid.videoWidth,
+        hi: vid.videoHeight,
+        ws: window.screen.availWidth,
+        hs: window.screen.availHeight,
+      });
 
-			win.setSize(size.w | 0, size.h | 0)
-			win.setMaximumSize(window.screen.availWidth, window.screen.availHeight)
-			win.center()
+      win.setSize(size.w | 0, size.h | 0);
+      win.setMaximumSize(window.screen.availWidth, window.screen.availHeight);
+      win.center();
 
-			transform.reset()
+      transform.reset();
 
-			win.show()
-		}
-	}
+      win.show();
+    }
+  };
 } else {
-	title.innerText = file
+  title.innerText = file;
 
-	img.src = `${dirname}${file}`
-	img.style.visibility = 'visible'
+  img.src = `${dirname}${file}`;
+  img.style.visibility = "visible";
 
-	img.onload = () => {
-		if (first) {
-			first = false
+  img.onload = () => {
+    if (first) {
+      first = false;
 
-			size = helper.setWindowSize({
-				wi: img.naturalWidth, hi: img.naturalHeight,
-				ws: window.screen.availWidth, hs: window.screen.availHeight
-			})
+      size = helper.setWindowSize({
+        wi: img.naturalWidth,
+        hi: img.naturalHeight,
+        ws: window.screen.availWidth,
+        hs: window.screen.availHeight,
+      });
 
-			win.setSize(size.w | 0, size.h | 0)
-			win.setMaximumSize(window.screen.availWidth, window.screen.availHeight)
-			win.center()
+      win.setSize(size.w | 0, size.h | 0);
+      win.setMaximumSize(window.screen.availWidth, window.screen.availHeight);
+      win.center();
 
-			transform.reset()
+      transform.reset();
 
-			win.show()
-		}
-	}
+      win.show();
+    }
+  };
 
-	img.onerror = e => {
-		alert(`Error opening file: \n\n${dirname}${file}`)
-		win.close()
-	}
+  img.onerror = (e) => {
+    alert(`Error opening file: \n\n${dirname}${file}`);
+    win.close();
+  };
 }
 
-document.ondragover = e => e.preventDefault()
-document.ondrop = e => e.preventDefault()
-document.onmousewheel = e => (e.wheelDelta > 0) ? transform.up() : transform.down()
+document.ondragover = (e) => e.preventDefault();
+document.ondrop = (e) => e.preventDefault();
+document.onmousewheel = (e) =>
+  e.wheelDelta > 0 ? transform.up() : transform.down();
 
-img.onmousedown = e => e.which === 1 && helper.move(e)
-vid.onmousedown = e => e.which === 1 && helper.move(e)
+img.onmousedown = (e) => e.which === 1 && helper.move(e);
+vid.onmousedown = (e) => e.which === 1 && helper.move(e);
 
 fs.readdir(path.dirname(sharedObj.filepath), (err, content) => {
+  content.sort(
+    (a, b) =>
+      fs.statSync(dirname + "/" + b).mtime.getTime() -
+      fs.statSync(dirname + "/" + a).mtime.getTime()
+  );
 
-	content.sort((a, b) =>
-		fs.statSync(dirname + '/' + b).mtime.getTime() -
-		fs.statSync(dirname + '/' + a).mtime.getTime()
-	)
+  const files = helper.iterator(content, content.indexOf(file));
 
-	const files = helper.iterator(content, content.indexOf(file))
+  title.innerText = file;
 
-	title.innerText = file
+  document.onkeydown = (e) => {
+    switch (e.code) {
+      case "KeyD":
+        return transform.rotate(1);
+      case "ArrowRight":
+        return (file = helper.iterate(files, path, dirname, transform, 1));
+      case "KeyA":
+        return transform.rotate(-1);
+      case "ArrowLeft":
+        return (file = helper.iterate(files, path, dirname, transform, -1));
+      case "KeyW":
+      case "ArrowUp":
+        return transform.up();
+      case "KeyS":
+      case "ArrowDown":
+        return transform.down();
+      case "Backspace":
+      case "Delete":
+        files.remove(`${dirname}${file}`);
+        return (file = helper.iterate(files, path, dirname, transform, 1));
+      case "Tab":
+        e.preventDefault();
+        return win.isMaximized() ? win.unmaximize() : win.maximize();
+      case "Escape":
+        return win.close();
+      default:
+        return;
+    }
+  };
 
-	document.onkeydown = e => {
-		switch (e.code) {
-			case 'KeyD':
-				return transform.rotate(1)
-			case 'ArrowRight':
-				return file = helper.iterate(files, path, dirname, transform, 1)
-			case 'KeyA':
-				return transform.rotate(-1)
-			case 'ArrowLeft':
-				return file = helper.iterate(files, path, dirname, transform, -1)
-			case 'KeyW':
-			case 'ArrowUp':
-				return transform.up()
-			case 'KeyS':
-			case 'ArrowDown':
-				return transform.down()
-			case 'Backspace':
-			case 'Delete':
-				files.remove(`${dirname}${file}`)
-				return file = helper.iterate(files, path, dirname, transform, 1)
-			case 'Tab':
-				e.preventDefault()
-				return win.isMaximized() ? win.unmaximize() : win.maximize()
-			case 'Escape': return win.close()
-			default:
-				return
-		}
-	}
-
-	document.onmousedown = ({ which }) => {
-		switch (which) {
-			case 5: return file = helper.iterate(files, path, dirname, transform, 1)
-			case 4: return file = helper.iterate(files, path, dirname, transform, -1)
-		}
-	}
-})
+  document.onmousedown = ({ which }) => {
+    switch (which) {
+      case 5:
+        return (file = helper.iterate(files, path, dirname, transform, 1));
+      case 4:
+        return (file = helper.iterate(files, path, dirname, transform, -1));
+    }
+  };
+});
