19fa8664d71a1e0085df626f65c2190b410d68d6
Daniel Nilsen <dafn@Daniels-MacBook-Pro.local>
2021-10-28 17:56:44 +0200

    clean

--- index.js
+++ index.js
@@ -1,65 +1,30 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
-// const { initIpcMain } = require("./src/js/ipc");
 const { INDEX } = require("./src/js/constants");
 
 let windows = [];
 global.seeit = {};
 
 createWindow = (env) => {
-  if (process.platform == "win32") {
+  if (process.platform == "win32" || process.env.NODE_ENV === "development") {
     global.seeit.filepath = process.argv[INDEX];
-
-    let window = new BrowserWindow({
-      minWidth: 128,
-      minHeight: 128,
-      autoHideMenuBar: true,
-      frame: false,
-      darkTheme: true,
-      backgroundColor: "#21252B",
-      show: process.env.NODE_ENV === "development",
-      webPreferences: { nodeIntegration: true, contextIsolation: false },
-    });
-
-    window.loadURL(`file://${__dirname}/src/view/index.html`);
-    window.on("closed", () => (win = null));
-
-    process.env.NODE_ENV === "development" && window.webContents.openDevTools();
-  } else {
-    if (process.env.NODE_ENV === "development") {
-      global.seeit.filepath = process.argv[INDEX];
-    }
-
-    let window = new BrowserWindow({
-      minWidth: 128,
-      minHeight: 128,
-      autoHideMenuBar: true,
-      titleBarStyle: "hidden",
-      darkTheme: true,
-      backgroundColor: "#21252B",
-      show: process.env.NODE_ENV === "development",
-      webPreferences: { nodeIntegration: true, contextIsolation: false },
-    });
-
-    window.loadURL(`file://${__dirname}/src/view/index.html`);
-
-    window.onbeforeunload = (e) => {
-      global.seeit.closingWindow = windows.findIndex(
-        (win) => win && !window.isDestroyed() && win.id === window.id
-      );
-      return true;
-    };
-
-    window.on("closed", (event) => {
-      if (global.seeit.closingWindow) {
-        windows[global.seeit.closingWindow] = null;
-      }
-      window = null;
-    });
-
-    process.env.NODE_ENV === "development" && window.webContents.openDevTools();
-
-    windows.push(window);
   }
+
+  let window = new BrowserWindow({
+    minWidth: 128,
+    minHeight: 128,
+    autoHideMenuBar: true,
+    frame: !process.platform == "win32",
+    darkTheme: true,
+    backgroundColor: "#21252B",
+    show: process.env.NODE_ENV === "development",
+    webPreferences: { nodeIntegration: true, contextIsolation: false },
+  });
+
+  window.loadURL(`file://${__dirname}/src/view/index.html`);
+
+  process.env.NODE_ENV === "development" && window.webContents.openDevTools();
+
+  windows.push(window);
 };
 
 if (process.platform == "darwin" && process.env.NODE_ENV !== "development") {
@@ -81,7 +46,7 @@ app.on("window-all-closed", app.quit);
 ipcMain.on(`remote`, (event, arg) => {
   const [command, ...args] = arg;
   const window = windows.find(
-    (window) => window && !window.isDestroyed() && window.id === event.sender.id
+    (window) => !window.isDestroyed() && window.id === event.sender.id
   );
 
   switch (command) {
--- src/js/ipc.js
+++ src/js/ipc.js
@@ -1,114 +1,15 @@
-const fs = require("fs");
-
-exports.initIpcMain = (ipc, window, env) => {
-  ipc.on(`${env.nr}`, (event, arg) => {
-    const [command, ...args] = arg;
-
-    // console.log(event.sender.id)
-
-    // fs.writeFileSync(
-    //   "/Users/dafn/Desktop/seeit.txt",
-    //   `senderId: ${event.sender.id} env: ${JSON.stringify(env)}`
-    // );
-
-    switch (command) {
-      case "filepath":
-        event.returnValue = env.filepath;
-        break;
-      case "platform":
-        event.returnValue = env.platform;
-        break;
-      default:
-        window[command](...args);
-        event.returnValue = null;
-        break;
-    }
-  });
-
-  // ipc.on("getNumber", (event) => {
-  //   event.returnValue = env.nr;
-  // });
-
-  /*
-  ipc.on("show", (event, arg) => {
-    window.show();
-    event.returnValue = null;
-  });
-
-  ipc.on("center", (event) => {
-    window.center();
-    event.returnValue = null;
-  });
-
-  ipc.on("setSize", (event, arg) => {
-    window.setSize(...arg);
-    event.returnValue = null;
-  });
-
-  ipc.on("setMaximumSize", (event, arg) => {
-    window.setMaximumSize(...arg);
-    event.returnValue = null;
-  });
-
-  ipc.on("close", (event) => {
-    window.close();
-    event.returnValue = null;
-  });
-
-  ipc.on("isMaximized", (event) => {
-    window.isMaximized();
-    event.returnValue = null;
-  });
-
-  ipc.on("maximize", (event) => {
-    window.maximize();
-    event.returnValue = null;
-  });
-
-  ipc.on("unmaximize", (event) => {
-    window.unmaximize();
-    event.returnValue = null;
-  });
-
-  ipc.on("filepath", (event) => {
-    event.returnValue = env.filepath;
-  });
-
-  ipc.on("platform", (event) => {
-    event.returnValue = env.platform;
-  });
-  */
-};
-
 exports.initIpcRenderer = (ipc = "remote") => {
   return {
-    show: () => ipcRenderer.sendSync(`${ipc}`, ["show"]),
-    close: () => ipcRenderer.sendSync(`${ipc}`, ["close"]),
-    center: () => ipcRenderer.sendSync(`${ipc}`, ["center"]),
-    setSize: (arg) => ipcRenderer.sendSync(`${ipc}`, ["setSize", ...arg]),
+    show: () => ipcRenderer.sendSync(ipc, ["show"]),
+    close: () => ipcRenderer.sendSync(ipc, ["close"]),
+    center: () => ipcRenderer.sendSync(ipc, ["center"]),
+    setSize: (arg) => ipcRenderer.sendSync(ipc, ["setSize", ...arg]),
     setMaximumSize: (arg) =>
-      ipcRenderer.sendSync(`${ipc}`, ["setMaximumSize", ...arg]),
-    maximize: () => ipcRenderer.sendSync(`${ipc}`, ["maximize"]),
-    isMaximized: () => ipcRenderer.sendSync(`${ipc}`, ["isMaximized"]),
-    unmaximize: () => ipcRenderer.sendSync(`${ipc}`, ["unmaximize"]),
-    filepath: () => ipcRenderer.sendSync(`${ipc}`, ["filepath"]),
-    platform: () => ipcRenderer.sendSync(`${ipc}`, ["platform"]),
+      ipcRenderer.sendSync(ipc, ["setMaximumSize", ...arg]),
+    maximize: () => ipcRenderer.sendSync(ipc, ["maximize"]),
+    isMaximized: () => ipcRenderer.sendSync(ipc, ["isMaximized"]),
+    unmaximize: () => ipcRenderer.sendSync(ipc, ["unmaximize"]),
+    filepath: () => ipcRenderer.sendSync(ipc, ["filepath"]),
+    platform: () => ipcRenderer.sendSync(ipc, ["platform"]),
   };
 };
-
-// exports.getNr = () => ipcRenderer.sendSync("getNumber");
-
-// exports.initIpcRenderer = () => {
-//   return {
-//     show: () => ipcRenderer.sendSync("show", "show"),
-//     close: () => ipcRenderer.sendSync("close", "close"),
-//     center: () => ipcRenderer.sendSync("center", "center"),
-//     setSize: (arg) => ipcRenderer.sendSync("setSize", arg),
-//     setMaximumSize: (arg) => ipcRenderer.sendSync("setMaximumSize", arg),
-//     maximize: () => ipcRenderer.sendSync("maximize", "maximize"),
-//     isMaximized: () => ipcRenderer.sendSync("isMaximized", "isMaximized"),
-//     unmaximize: () => ipcRenderer.sendSync("unmaximize", "unmaximize"),
-//     filepath: () => ipcRenderer.sendSync("filepath", "filepath"),
-//     platform: () => ipcRenderer.sendSync("platform", "platform"),
-//   };
-// };
